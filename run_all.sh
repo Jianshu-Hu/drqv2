6392deac70e5d114a5d4aa5238e4ceb73c4a8a7e
Liu Zhanxun <xcc_zach@sjtu.edu.cn>
2023-10-17 12:05:48 +0800

    run_all script

---
+++ run_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+bash run_single.sh acrobot_swingup 42
+bash run_single.sh acrobot_swingup 43
+bash run_single.sh acrobot_swingup 44
+bash run_single.sh acrobot_swingup 45
+bash run_single.sh acrobot_swingup 46
+bash run_single.sh reacher_hard 42
+bash run_single.sh reacher_hard 43
+bash run_single.sh reacher_hard 44
+bash run_single.sh reacher_hard 45
+bash run_single.sh reacher_hard 46
+bash run_ringle.sh walker_run 42
+bash run_ringle.sh walker_run 43
+bash run_ringle.sh walker_run 44
+bash run_ringle.sh walker_run 45
+bash run_ringle.sh walker_run 46
+
+
+
