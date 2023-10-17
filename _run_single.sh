e8a52e98b3e336b63687612e0501e02dc1844b03
Liu Zhanxun <xcc_zach@sjtu.edu.cn>
2023-10-17 23:51:02 +0800

    new run single

---
+++ _run_single.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+task=$1
+seed=$2
+num_train_frames=1000000
+replay_buffer_num_workers=1
+
+echo "-------------------------------------------------"
+echo "task=$task, seed=$seed, num_train_frames=$num_train_frames, replay_buffer_num_workers=$replay_buffer_num_workers"
+
+current_date=$(date +%Y.%m.%d)
+echo "Current date: $current_date"
+result_source_folder="exp_local/$current_date"
+echo "Result source folder: $result_source_folder"
+
+save_results_foler="saved_exps"
+echo "Save results folder: $save_results_foler"
+if [ ! -d "$save_results_foler" ]; then
+    mkdir $save_results_foler
+    echo "Created save results folder: $save_results_foler"
+fi
+
+task_result_folder="$task-$seed-$current_date"
+mkdir "$save_results_foler/$task_result_folder"
+
+for aug_type in {1..11}
+do
+    echo "Start running $task with seed $seed and aug_type $aug_type"
+    python train.py task=$task experiment=$task seed=$seed replay_buffer_num_workers=$replay_buffer_num_workers num_train_frames=$num_train_frames aug_type=$aug_type
+    mkdir "$save_results_foler/$task_result_folder/aug$aug_type"
+    mv $result_source_folder/* $save_results_foler/$task_result_folder/aug$aug_type/
+    echo "Done running task $task with seed $seed and aug_type $aug_type, results saved in $save_results_foler/$task_result_folder/aug$aug_type"
+done
--- run_single.sh
+++ run_single.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
 task=$1
-seed=$2
+aug_type=$2
+seed=$3
+identifier=$4
 num_train_frames=1000000
 replay_buffer_num_workers=1
 
-echo "-------------------------------------------------"
-echo "task=$task, seed=$seed, num_train_frames=$num_train_frames, replay_buffer_num_workers=$replay_buffer_num_workers"
+echo "identifier $identifier: task=$task, aug_type=$aug_type, seed=$seed, num_train_frames=$num_train_frames, replay_buffer_num_workers=$replay_buffer_num_workers"
 
-current_date=$(date +%Y.%m.%d)
-echo "Current date: $current_date"
-result_source_folder="exp_local/$current_date"
-echo "Result source folder: $result_source_folder"
+python train.py task=$task experiment=$task aug_type=$aug_type seed=$seed replay_buffer_num_workers=$replay_buffer_num_workers num_train_frames=$num_train_frames
 
 save_results_foler="saved_exps"
-echo "Save results folder: $save_results_foler"
 if [ ! -d "$save_results_foler" ]; then
     mkdir $save_results_foler
     echo "Created save results folder: $save_results_foler"
 fi
 
-task_result_folder="$task-$seed-$current_date"
-mkdir "$save_results_foler/$task_result_folder"
+if [ ! -d "$save_results_foler/$task-$identifier" ]; then
+    mkdir "$save_results_foler/$task-$identifier"
+    echo "Created save results folder: $save_results_foler//$task-$identifier"
+fi
 
-for aug_type in {1..11}
-do
-    echo "Start running $task with seed $seed and aug_type $aug_type"
-    python train.py task=$task experiment=$task seed=$seed replay_buffer_num_workers=$replay_buffer_num_workers num_train_frames=$num_train_frames aug_type=$aug_type
-    mkdir "$save_results_foler/$task_result_folder/aug$aug_type"
-    mv $result_source_folder/* $save_results_foler/$task_result_folder/aug$aug_type/
-    echo "Done running task $task with seed $seed and aug_type $aug_type, results saved in $save_results_foler/$task_result_folder/aug$aug_type"
-done
+current_date=$(date +%Y.%m.%d)
+source_folder="exp_local/$current_date"
+
+mkdir "$save_results_foler/$task-$identifier/aug$aug_type"
+mv $source_folder/* $save_results_foler/$task-$identifier/aug$aug_type/
