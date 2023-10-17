d1df77b7f9d05bc0bdc42f08184fab2632eac0e6
Liu Zhanxun <xcc_zach@sjtu.edu.cn>
2023-10-18 00:33:50 +0800

    update single task

--- run_single.sh
+++ run_single.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source /bigdata/users/jhu/anaconda3/bin/activate
-conda activate equiRL
+# source /bigdata/users/jhu/anaconda3/bin/activate
+# conda activate equiRL
 
 task=$1
 aug_type=$2
@@ -11,7 +11,10 @@ replay_buffer_num_workers=1
 
 echo "identifier $identifier: task=$task, aug_type=$aug_type, seed=$seed, num_train_frames=$num_train_frames, replay_buffer_num_workers=$replay_buffer_num_workers"
 
-python train.py task=$task experiment=$task aug_type=$aug_type seed=$seed replay_buffer_num_workers=$replay_buffer_num_workers num_train_frames=$num_train_frames
+current_date=$(date +%Y.%m.%d)
+source_folder="exp_local/$current_date"
+
+# python train.py task=$task experiment=$task aug_type=$aug_type seed=$seed replay_buffer_num_workers=$replay_buffer_num_workers num_train_frames=$num_train_frames
 
 save_results_foler="saved_exps"
 if [ ! -d "$save_results_foler" ]; then
@@ -24,8 +27,9 @@ if [ ! -d "$save_results_foler/$task-$seed-$identifier" ]; then
     echo "Created save results folder: $save_results_foler//$task-$seed-$identifier"
 fi
 
-current_date=$(date +%Y.%m.%d)
-source_folder="exp_local/$current_date"
-
 mkdir "$save_results_foler/$task-$seed-$identifier/aug$aug_type"
-mv $source_folder/* $save_results_foler/$task-$seed-$identifier/aug$aug_type/
+find_aug_type="*aug_type=$aug_type*"
+find_seed="*seed=$seed*"
+find_task="*task=$task*"
+result_folder=$(find $source_folder -type d -name $find_aug_type -name $find_task -name $find_seed | head -n 1)
+mv $result_folder $save_results_foler/$task-$seed-$identifier/aug$aug_type/
