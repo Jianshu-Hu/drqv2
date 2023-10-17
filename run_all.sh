91c24632d67da4d04ac86c4ac9b9ee8966610fd6
Liu Zhanxun <xcc_zach@sjtu.edu.cn>
2023-10-17 13:31:42 +0800

    bug fix

--- run_all.sh
+++ run_all.sh
@@ -9,11 +9,11 @@ bash run_single.sh reacher_hard 43
 bash run_single.sh reacher_hard 44
 bash run_single.sh reacher_hard 45
 bash run_single.sh reacher_hard 46
-bash run_ringle.sh walker_run 42
-bash run_ringle.sh walker_run 43
-bash run_ringle.sh walker_run 44
-bash run_ringle.sh walker_run 45
-bash run_ringle.sh walker_run 46
+bash run_single.sh walker_run 42
+bash run_single.sh walker_run 43
+bash run_single.sh walker_run 44
+bash run_single.sh walker_run 45
+bash run_single.sh walker_run 46
 
 
 
