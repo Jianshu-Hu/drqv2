4109a4b3025d3c1d21138a33614f509febd9dd5f
GOK-656 <ivanli656@163.com>
2023-10-18 22:34:10 -0400

    5 seeds

--- run_experiments/lyf_experiments/run_cluster_reacher_hard_drq_1.sh
+++ run_experiments/lyf_experiments/run_cluster_reacher_hard_drq_1.sh
@@ -9,3 +9,19 @@ seed=1
 
 echo "start running $tag with seed $seed"
 python train.py aug_type=2 task=reacher_hard experiment=$tag seed=$seed replay_buffer_num_workers=4 num_train_frames=1000000
+seed=2
+
+echo "start running $tag with seed $seed"
+python train.py aug_type=2 task=reacher_hard experiment=$tag seed=$seed replay_buffer_num_workers=4 num_train_frames=1000000
+seed=3
+
+echo "start running $tag with seed $seed"
+python train.py aug_type=2 task=reacher_hard experiment=$tag seed=$seed replay_buffer_num_workers=4 num_train_frames=1000000
+seed=4
+
+echo "start running $tag with seed $seed"
+python train.py aug_type=2 task=reacher_hard experiment=$tag seed=$seed replay_buffer_num_workers=4 num_train_frames=1000000
+seed=5
+
+echo "start running $tag with seed $seed"
+python train.py aug_type=2 task=reacher_hard experiment=$tag seed=$seed replay_buffer_num_workers=4 num_train_frames=1000000
