9005f8978c2854ff74952ac97d260cc2c9a893ae
GOK-656 <ivanli656@163.com>
2023-10-21 13:20:53 -0400

    add acrobot and walker

---
+++ run_experiments/lyf_experiments/run_cluster_walker_run_drq_1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd /bigdata/users/ve490-fall23/lyf/drqv2/
+source /bigdata/users/jhu/anaconda3/bin/activate
+conda activate equiRL
+
+tag=walker_run
+seed=1
+
+echo "start running $tag with seed $seed"
+python train.py aug_type=2 task=reacher_hard experiment=$tag seed=$seed replay_buffer_num_workers=4 num_train_frames=1000000
+seed=2
+
+echo "start running $tag with seed $seed"
+python train.py aug_type=2 task=reacher_hard experiment=$tag seed=$seed replay_buffer_num_workers=4 num_train_frames=1000000
+seed=3
+
+echo "start running $tag with seed $seed"
+python train.py aug_type=2 task=reacher_hard experiment=$tag seed=$seed replay_buffer_num_workers=4 num_train_frames=1000000
+seed=4
+
+echo "start running $tag with seed $seed"
+python train.py aug_type=2 task=reacher_hard experiment=$tag seed=$seed replay_buffer_num_workers=4 num_train_frames=1000000
+seed=5
+
+echo "start running $tag with seed $seed"
+python train.py aug_type=2 task=reacher_hard experiment=$tag seed=$seed replay_buffer_num_workers=4 num_train_frames=1000000
