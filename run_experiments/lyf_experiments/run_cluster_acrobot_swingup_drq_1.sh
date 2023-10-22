41b56f87740d36e1aebbd7214b0f939cb9623ab0
GOK-656 <ivanli656@163.com>
2023-10-22 19:15:06 -0400

    modify sh

--- run_experiments/lyf_experiments/run_cluster_acrobot_swingup_drq_1.sh
+++ run_experiments/lyf_experiments/run_cluster_acrobot_swingup_drq_1.sh
@@ -8,20 +8,20 @@ tag=acrobot_swingup
 seed=1
 
 echo "start running $tag with seed $seed"
-python train.py aug_type=2 task=reacher_hard experiment=$tag seed=$seed replay_buffer_num_workers=4 num_train_frames=1000000
+python train.py aug_type=2 task=acrobot_swingup experiment=$tag seed=$seed replay_buffer_num_workers=4 num_train_frames=1000000
 seed=2
 
 echo "start running $tag with seed $seed"
-python train.py aug_type=2 task=reacher_hard experiment=$tag seed=$seed replay_buffer_num_workers=4 num_train_frames=1000000
+python train.py aug_type=2 task=acrobot_swingup experiment=$tag seed=$seed replay_buffer_num_workers=4 num_train_frames=1000000
 seed=3
 
 echo "start running $tag with seed $seed"
-python train.py aug_type=2 task=reacher_hard experiment=$tag seed=$seed replay_buffer_num_workers=4 num_train_frames=1000000
+python train.py aug_type=2 task=acrobot_swingup experiment=$tag seed=$seed replay_buffer_num_workers=4 num_train_frames=1000000
 seed=4
 
 echo "start running $tag with seed $seed"
-python train.py aug_type=2 task=reacher_hard experiment=$tag seed=$seed replay_buffer_num_workers=4 num_train_frames=1000000
+python train.py aug_type=2 task=acrobot_swingup experiment=$tag seed=$seed replay_buffer_num_workers=4 num_train_frames=1000000
 seed=5
 
 echo "start running $tag with seed $seed"
-python train.py aug_type=2 task=reacher_hard experiment=$tag seed=$seed replay_buffer_num_workers=4 num_train_frames=1000000
+python train.py aug_type=2 task=acrobot_swingup experiment=$tag seed=$seed replay_buffer_num_workers=4 num_train_frames=1000000
